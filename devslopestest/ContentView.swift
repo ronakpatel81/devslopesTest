96e8f59db2f9195b3eb5fbe1fdaa1996ab066913
Ronak Patel <ronakpatel81@gmail.com>
2020-05-16 09:24:45 -0500

    hellow world

---
+++ devslopestest/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  devslopestest
+//
+//  Created by Ronak Patel on 5/16/20.
+//  Copyright © 2020 Ronak Patel. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World Test Ronak!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
